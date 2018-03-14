d878f4e7502c508ee57c6472260db7c6e6b2d112
kallom <k.madjunov@gmail.com>
2018-03-14 17:24:22 +0200

    Add hrefs and images

--- src/main/java/healthblog/controllers/HomeController.java
+++ src/main/java/healthblog/controllers/HomeController.java
@@ -27,5 +27,105 @@ public class HomeController {
 
         return "base-layout";
     }
+
+    @GetMapping("/fitness")
+    public String fitness(Model model) {
+        List<Article> articles = this.articleRepository.findAll();
+
+        model.addAttribute("view", "home/fitness");
+        model.addAttribute("articles", articles);
+
+        return "base-layout";
+    }
+
+    @GetMapping("/fitness/challenges")
+    public String challenges(Model model) {
+        model.addAttribute("view", "home/fitness-challenges");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/fitness/exercises")
+    public String exercises(Model model) {
+        model.addAttribute("view", "home/fitness-exercises");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/food")
+    public String food(Model model) {
+        List<Article> articles = this.articleRepository.findAll();
+
+        model.addAttribute("view", "home/food");
+        model.addAttribute("articles", articles);
+
+        return "base-layout";
+    }
+
+    @GetMapping("/food/healthy")
+    public String healthy(Model model) {
+        model.addAttribute("view", "home/food-healthy");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/food/desserts")
+    public String desserts(Model model) {
+        model.addAttribute("view", "home/food-desserts");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/lifestyle")
+    public String lifestyle(Model model) {
+        List<Article> articles = this.articleRepository.findAll();
+
+        model.addAttribute("view", "home/lifestyle");
+        model.addAttribute("articles", articles);
+
+        return "base-layout";
+    }
+
+    @GetMapping("/lifestyle/wear-it-better")
+    public String wearItBetter(Model model) {
+        model.addAttribute("view", "home/lifestyle-wear-it-better");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/lifestyle/motivation")
+    public String motivation(Model model) {
+        model.addAttribute("view", "home/lifestyle-motivation");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("view", "home/about");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/about/my-transformation")
+    public String myTransformation(Model model) {
+        model.addAttribute("view", "home/about-my-transformation");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/about/me-myself-and-i")
+    public String meMyselfAndI(Model model) {
+        model.addAttribute("view", "home/about-me-myself-and-i");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("view", "home/contact");
+
+        return "base-layout";
+    }
 }
 
